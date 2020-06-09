b2b397c1c9a78f7fa7e7cb0eccadd210ec434592
Сергей Пегов <sergei.pegov@stecpoint.ru>
2020-06-09 22:27:57 +0300

    paging

--- src/app/vehicle/vehicle-home/vehicle-home.component.ts
+++ src/app/vehicle/vehicle-home/vehicle-home.component.ts
@@ -1,10 +1,11 @@
 import {Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {HttpServiceService} from "../../services/http-service.service";
 import {Vehicle} from "../../models/Vehicle";
-import {Observable} from 'rxjs';
+import {Observable, range} from 'rxjs';
 import {AlertService} from "../../services/alert.service";
 import {PageContext, VehiclePageFilter} from "../../models/pageContext";
 import {ModalPubSubService} from "../../services/modal-pub-sub.service";
+import {formatNumber} from "@angular/common";
 
 @Component({
   selector: 'app-vehicle-home',
@@ -17,8 +18,12 @@ export class VehicleHomeComponent implements OnInit {
   isLoaded:boolean = true;
   vehicles: Vehicle[];
   totalCount: number;
-  totalPages: Array<number>;
-  currentPage:number;
+  pages: Array<number>;
+  totalPages:number = 0;
+  pageSize:number = 12;
+  currentPage:number  = 1;
+  currentMinRange:number = 1;
+  currentMaxRange:number = 10;
 
   //@ViewChild('modalRef', {static: false,  read: ViewContainerRef}) containerRef: ViewContainerRef;
   //@ViewChild(ModalRefDirective, {static: false,  read: ViewContainerRef}) containerRef: ViewContainerRef;
@@ -26,12 +31,11 @@ export class VehicleHomeComponent implements OnInit {
   constructor(
     private httpClient: HttpServiceService,
     private alertService: AlertService,
-    private modalPubSubService: ModalPubSubService
-     ) { }
+    private modalPubSubService: ModalPubSubService) { }
 
   ngOnInit(): void {
     this.isLoaded = false;
-    this.getData(1, 12);
+    this.getData(1, this.pageSize);
   }
 
   // showModal(){
@@ -65,33 +69,87 @@ export class VehicleHomeComponent implements OnInit {
       .subscribe(data => {
         this.vehicles = data.data;
         this.totalCount = data.totalCount;
+        this.totalPages = Math.ceil(data.totalCount / this.pageSize);
         this.isLoaded = true;
-        this.setPages(data.totalCount);
-
+        this.fillRange();
         this.alertService.success('Title', 'Данные обновились','' );
       });
   }
-  setPages(totalCount:number){
-    let count: number;
 
-    if(totalCount > 10){
-      count = 10;
-    }else {
-      count = totalCount;
+  fillRange(){
+    let step: number = 9;
+
+    if(this.currentPage === this.currentMinRange){
+      this.pages = new Array<number>(9);
+      let indx:number = this.currentMinRange - 1;
+
+      for (let i = 0; i <= step; i++){
+        indx = indx + 1;
+        this.pages[i] = indx;
+      }
     }
 
-    this.totalPages = new Array<number>(count);
-    for (let i=0; i<=count; i++){
-      this.totalPages[i] = (i+1);
+    if(this.currentPage > this.currentMaxRange){
+
+      this.currentMinRange = this.currentMaxRange + 1;
+      this.currentMaxRange = this.currentMinRange + 9;
+      this.pages = [];
+
+      let indx:number = this.currentMinRange - 1;
+
+      if(this.currentMaxRange >= this.totalPages){
+        step = this.totalPages - (this.currentMinRange);
+      }
+
+      this.pages = new Array<number>(step);
+      for (let i = 0; i <= step; i++){
+        indx = indx + 1;
+        this.pages[i] = indx;
+      }
+    }
+
+    if(this.currentPage < this.currentMinRange){
+
+      this.currentMaxRange = this.currentMinRange - 1;
+      this.currentMinRange = this.currentMaxRange - 9;
+
+      this.pages = [];
+
+      let indx:number = this.currentMinRange - 1;
+
+      if(this.currentMaxRange >= this.totalPages){
+        step = this.totalPages - (this.currentMinRange);
+      }
+
+      this.pages = new Array<number>(step);
+      for (let i = 0; i <= step; i++){
+        indx = indx + 1;
+        this.pages[i] = indx;
+      }
     }
   }
 
   onCurrentPage(event){
-    this.currentPage = event;
-    this.getData(event, 12);
+    this.currentPage = Number(event);
+    this.getData(this.currentPage, this.pageSize);
+  }
+
+  onBack(){
+    if(this.currentPage !==1){
+      this.currentPage = this.currentPage - 1;
+      this.getData(this.currentPage, this.pageSize);
+    }
+  }
+
+  onNext(){
+    if(this.currentPage !==this.totalCount){
+      this.currentPage = this.currentPage + 1;
+      this.getData(this.currentPage, this.pageSize);
+    }
   }
 
   showModal(event) {
     this.modalPubSubService.showDialogModal(event);
   }
 }
+
